927cd8fedc5c130ec179d7de063eb1b214710103
Laura Verdoes <1766841+Vivelin@users.noreply.github.com>
2023-12-29 09:11:32 +0100

    Move alarm to foreground service

---
+++ Platforms/Android/AlarmMediaService.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+
+using AndroidX.Core.App;
+
+namespace MauiCatAlarm.Platforms.Android;
+
+[Service(ForegroundServiceType = ForegroundService.TypeMediaPlayback)]
+public class AlarmMediaService : Service
+{
+    public const int NotificationId = 69;
+
+    public override IBinder? OnBind(Intent? intent) => null;
+
+    [return: GeneratedEnum]
+    public override StartCommandResult OnStartCommand(Intent? intent, [GeneratedEnum] StartCommandFlags flags, int startId)
+    {
+        var context = ApplicationContext ?? throw new InvalidOperationException("Context not available.");
+
+        // if (OperatingSystem.IsAndroidVersionAtLeast(33))
+        // {
+        //     var notificationPermission = context.CheckSelfPermission(global::Android.Manifest.Permission.PostNotifications);
+        //     if (notificationPermission == Permission.Denied)
+        //     {
+        //         StopSelf();
+        //         return StartCommandResult.NotSticky;
+        //     }
+        // }
+
+        var alarmIntent = new Intent(context, typeof(AlarmActivity));
+        alarmIntent.AddFlags(ActivityFlags.NewTask | ActivityFlags.ClearTask);
+        var pendingIntent = PendingIntent.GetActivity(context, 0, alarmIntent, PendingIntentFlags.Immutable);
+
+        // var notificationManager = context.GetSystemService(Context.NotificationService).JavaCast<NotificationManager>();
+        var notification = new NotificationCompat.Builder(context, MainApplication.ChannelId)
+            .SetSmallIcon(Resource.Mipmap.appicon)
+            .SetContentTitle("CAT ALARM!")
+            .SetContentText("Mew mew mew mew mew mew!")
+            .SetPriority(NotificationCompat.PriorityHigh)
+            .SetCategory(NotificationCompat.CategoryAlarm)
+            .SetContentIntent(pendingIntent)
+            .SetFullScreenIntent(pendingIntent, true)
+            .SetOngoing(true)
+            .Build();
+
+        // notificationManager!.Notify(NotificationId, notification);
+
+        StartForeground(NotificationId, notification);
+        return StartCommandResult.Sticky;
+    }
+}
--- Platforms/Android/AlarmReceiver.cs
+++ Platforms/Android/AlarmReceiver.cs
@@ -10,30 +10,13 @@ namespace MauiCatAlarm.Platforms.Android;
 [BroadcastReceiver(Exported = true, Enabled = true)]
 public class AlarmReceiver : BroadcastReceiver
 {
-    public const int NotificationId = 69;
-
     public override void OnReceive(Context? context, Intent? intent)
     {
         Log.Info("AlarmReceiver", "Alarm received");
         if (context == null)
             throw new InvalidOperationException("Context is null");
 
-        var alarmIntent = new Intent(context, typeof(AlarmActivity));
-        alarmIntent.AddFlags(ActivityFlags.NewTask | ActivityFlags.ClearTask);
-        var pendingIntent = PendingIntent.GetActivity(context, 0, alarmIntent, PendingIntentFlags.Immutable);
-
-        var notificationManager = context.GetSystemService(Context.NotificationService).JavaCast<NotificationManager>();
-        var notification = new NotificationCompat.Builder(context, MainApplication.ChannelId)
-            .SetSmallIcon(Resource.Mipmap.appicon)
-            .SetContentTitle("CAT ALARM!")
-            .SetContentText("Mew mew mew mew mew mew!")
-            .SetPriority(NotificationCompat.PriorityHigh)
-            .SetCategory(NotificationCompat.CategoryAlarm)
-            .SetContentIntent(pendingIntent)
-            .SetFullScreenIntent(pendingIntent, true)
-            .SetOngoing(true)
-            .Build();
-
-        notificationManager!.Notify(NotificationId, notification);
+        var mediaIntent = new Intent(context, typeof(AlarmMediaService));
+        context.StartForegroundService(mediaIntent);
     }
 }
