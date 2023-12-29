ae60148fe3df22b8034cf7bc49dfdfe851d35722
Laura Verdoes <1766841+Vivelin@users.noreply.github.com>
2023-12-29 10:25:44 +0100

    IT'S WORKING! IT'S WORKING!

--- Platforms/Android/AlarmMediaService.cs
+++ Platforms/Android/AlarmMediaService.cs
@@ -1,8 +1,10 @@
 ﻿using Android.App;
 using Android.Content;
 using Android.Content.PM;
+using Android.Media;
 using Android.OS;
 using Android.Runtime;
+using Android.Util;
 
 using AndroidX.Core.App;
 
@@ -13,11 +15,38 @@ public class AlarmMediaService : Service
 {
     public const int NotificationId = 69;
 
+    private MediaPlayer? _player;
+
+    public override void OnCreate()
+    {
+        Log.Info("AlarmMediaService", "OnCreate Thread=" + Java.Lang.Thread.CurrentThread().Id);
+        base.OnCreate();
+
+        var context = ApplicationContext ?? throw new InvalidOperationException("Context not available.");
+
+        var audioManager = context.GetSystemService(AudioService).JavaCast<AudioManager>();
+        var session = audioManager!.GenerateAudioSessionId();
+
+        var uri = RingtoneManager.GetDefaultUri(RingtoneType.Alarm);
+        var attrib = new AudioAttributes.Builder()
+            .SetContentType(AudioContentType.Music)
+            !.SetUsage(AudioUsageKind.Alarm)
+            !.Build();
+
+        var callbacks = new PlayerCallbacks();
+        _player = MediaPlayer.Create(context, uri, null, attrib, session);
+        _player!.Looping = true;
+        _player.SetOnPreparedListener(callbacks);
+        _player.SetOnInfoListener(callbacks);
+        _player.SetOnErrorListener(callbacks);
+    }
+
     public override IBinder? OnBind(Intent? intent) => null;
 
     [return: GeneratedEnum]
     public override StartCommandResult OnStartCommand(Intent? intent, [GeneratedEnum] StartCommandFlags flags, int startId)
     {
+        Log.Info("AlarmMediaService", $"OnStartCommand {intent} {flags} {startId} Thread=" + Java.Lang.Thread.CurrentThread().Id);
         var context = ApplicationContext ?? throw new InvalidOperationException("Context not available.");
 
         // if (OperatingSystem.IsAndroidVersionAtLeast(33))
@@ -50,5 +79,40 @@ public class AlarmMediaService : Service
 
         StartForeground(NotificationId, notification);
         return StartCommandResult.Sticky;
+    }
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+        _player?.Stop();
+        _player?.Dispose();
+    }
+
+    public void Stop()
+    {
+        _player?.Stop();
+        StopForeground(StopForegroundFlags.Remove);
+        StopSelf();
+    }
+
+    private class PlayerCallbacks : Java.Lang.Object, MediaPlayer.IOnErrorListener, MediaPlayer.IOnPreparedListener, MediaPlayer.IOnInfoListener
+    {
+        public bool OnError(MediaPlayer? mp, [GeneratedEnum] MediaError what, int extra)
+        {
+            Log.Error("AlarmMediaService", $"MediaPlayer {mp.AudioSessionId} Error: {what} {extra}");
+            return true;
+        }
+
+        public bool OnInfo(MediaPlayer? mp, [GeneratedEnum] MediaInfo what, int extra)
+        {
+            Log.Info("AlarmMediaService", $"MediaPlayer {mp.AudioSessionId} {what} {extra}");
+            return true;
+        }
+
+        public void OnPrepared(MediaPlayer? mp)
+        {
+            Log.Info("AlarmMediaService", $"MediaPlayer {mp.AudioSessionId} prepared Thread=" + Java.Lang.Thread.CurrentThread().Id);
+            mp.Start();
+        }
     }
 }
